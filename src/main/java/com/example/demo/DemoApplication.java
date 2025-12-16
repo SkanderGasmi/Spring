47422d82ccfcbe258c4a65546482dcd5bfde6b58
Skander Gasmi <Skandergasmi7@gmail.com>
2025-12-16 21:23:39 +0100

    Initialize Spring Boot project with basic metadata and dependencies

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("helloWorld ");
+		System.out.println("helloWorld! ");
 	}
 
 }
